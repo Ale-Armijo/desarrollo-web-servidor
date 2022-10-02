6b787366f42ea9c05bf48de11eee4e7c2892ed69
Ale-Armijo <92530180+Ale-Armijo@users.noreply.github.com>
2022-10-02 20:33:01 +0200

    Commit Inicial

--- base_de_datos/modificar_pelicula.php
+++ base_de_datos/modificar_pelicula.php
@@ -1,36 +1,57 @@
 <?php
     require 'database.php';
-    if ($_SERVER["REQUEST_METHOD"]=="POST"
-        && $_POST["formulario"]=="modificar"){
-        // update peliculas Set bla bla where id=2
-        $id= $_POST["id"];
-        
-        $sql="SELECT * FROM peliculas WHERE id='$id'";
-        $resultado= $conexion -> query($sql);
-        if($resultado ->num_rows > 0){
-            while($row=$resultado->fetch_assoc()){
-                $titulo= $row["titulo"];
-                $duracion= $row["duracion"];
-                $fecha_estreno= $row["fecha_estreno"];
+    if ($_SERVER ["REQUEST_METHOD"]== "POST"
+            && $_POST["formulario"]== "actualizar"){   
+        $id = $_POST["id"];
+        $titulo= $_POST["titulo"];
+        $duracion=$_POST["duracion"];
+        $fecha_estreno= $_POST["fecha_estreno"];
+
+        $sql = "UPDATE peliculas 
+            SET titulo= '$titulo',
+                fecha_estreno='$fecha_estreno',
+                duracion='$duracion'
+            WHERE id='$id'";
+
+        if($conexion -> query($sql)== TRUE){
+            ECHO "<p>Pelicula modificada</p>";
+        }else{
+            ECHO "<p>Error al modificar</p>";
+        }
+        }
+    if ($_SERVER["REQUEST_METHOD"] == "POST"
+            && $_POST["formulario"] == "modificar") {
+
+        $id = $_POST["id"];
+
+        $sql = "SELECT * FROM peliculas WHERE id = '$id'";
+
+        $resultado = $conexion -> query($sql);
+
+        if ($resultado -> num_rows > 0) {
+            while ($row = $resultado -> fetch_assoc()) {
+                $titulo = $row["titulo"];
+                $duracion = $row["duracion"];
+                $fecha_estreno = $row["fecha_estreno"];
             }
         }
     }
+
 ?>
-if ($_SERVER["REQUEST_METHOD"]=="POST"
-    && $_POST["formulario"]=="actualizar"){
 
-    }
-<h2>Modificar pelicula<?php if (isset ($id)) echo $id;?></h2>
+<h2>Modificar película <?php if (isset($id)) echo $id; ?></h2>
 
-<form action="index.php" method="post">
-    Titulo: <input type="text" name="titulo" value="<?php echo $titulo?>">
+<form action="" method="post">
+    Título: <input type="text" name="titulo" value="<?php echo $titulo ?>">
     <br><br>
-    Duracion: <input type="text" name="duracion" value="<?php echo $duracion?>">
+    Duración: <input type="text" name="duracion" value="<?php echo $duracion ?>">
     <br><br>
     Fecha de estreno:<br>
-    <input type="date" name="fecha_estreno" value="<?php echo $fecha_estreno?>">
+    <input type="date" name="fecha_estreno" value="<?php echo $fecha_estreno ?>">
     <br><br>
-    <input type="hidden" name="formulario" value="actualizar">
-    <input type="submit" name="Modificar">
-
+    <input type= "hidden" name="id" value="<?php echo $id?>">
+    <input type= "hidden" name="formulario" value="actualizar">
+    <input type= "submit" value="Modificar">
 </form>
+<br><br>
+<a href= "index.php">Volver al inicio</a>
