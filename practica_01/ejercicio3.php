fe318e82543e2a15d3ada7aa2be4c5b9bd1fe07d
Ale-Armijo <92530180+Ale-Armijo@users.noreply.github.com>
2022-10-24 18:09:30 +0200

    nuevo

--- practica_01/ejercicio3.php
+++ practica_01/ejercicio3.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel= "stylesheet" type= "text/css" href= "stilo.css">
+    <link rel= "stylesheet" type= "text/css" href= "estiloMultiplicar.css">
     <title>Tablas de multiplicar</title>
 </head>
 <body>
@@ -18,7 +18,7 @@
             echo $a;
         }
         echo "<br>";
-    }*/
+    }
 
     for($i=1;$i<=4;$i++){
 
@@ -27,7 +27,7 @@
             echo " *";
         }
         echo "<br>";
-    }
+    }*/
 
     for($i=1; $i<=10;$i++){
         echo "<table id='tabla" . $i . "'>";
